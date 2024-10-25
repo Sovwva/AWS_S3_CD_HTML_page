c35e5be14c017db35ee6c6cd1488f8d7e72764c9
sovwva <sovwva7@gmail.com>
2024-10-25 21:31:38 +0200

    rewrite terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,28 +17,13 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "arn:aws:s3:::${data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_s3_bucket_object" "html_files" {
   for_each = fileset("html_files", "*.html")
 
   bucket = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
   key    = each.key
   source = "html_files/${each.key}"
-  acl    = "public-read"
+  acl    = "public-read"  # This allows individual objects to be publicly readable
 }
 
 output "website_url" {
