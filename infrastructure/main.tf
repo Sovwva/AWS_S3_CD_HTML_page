619204c93e1ccf9e9e8a89dd4aac36be1c906971
sovwva <sovwva7@gmail.com>
2024-10-25 21:33:30 +0200

    rewrite terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ data "aws_s3_bucket" "existing_bucket" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  count = data.aws_s3_bucket.existing_bucket.id != "" ? 0 : 1
+  count = length(try(data.aws_s3_bucket.existing_bucket.id, [])) == 0 ? 1 : 0
 
   bucket = "sovwva-aws-cd-html-bucket"
 
@@ -17,16 +17,31 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = length(try(data.aws_s3_bucket.existing_bucket.id, [])) > 0 ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::${length(try(data.aws_s3_bucket.existing_bucket.id, [])) > 0 ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_object" "html_files" {
   for_each = fileset("html_files", "*.html")
 
-  bucket = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
+  bucket = length(try(data.aws_s3_bucket.existing_bucket.id, [])) > 0 ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
   key    = each.key
   source = "html_files/${each.key}"
-  acl    = "public-read"  # This allows individual objects to be publicly readable
+  acl    = "public-read"
 }
 
 output "website_url" {
-  value = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket.website_bucket[0].website_endpoint
+  value = length(try(data.aws_s3_bucket.existing_bucket.id, [])) > 0 ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket.website_bucket[0].website_endpoint
   description = "URL of the website hosted on S3"
 }
