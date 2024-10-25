711a10df0697562332176232489ace5334bdace1
sovwva <sovwva7@gmail.com>
2024-10-25 21:47:29 +0200

    rewrite terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,18 +6,13 @@ locals {
   bucket_name = "sovwva-aws-cd-html-bucket"
 }
 
-# Попытка получить существующий бакет
 data "aws_s3_bucket" "existing_bucket" {
+  count = length(try(aws_s3_bucket.existing_bucket.id, null)) > 0 ? 1 : 0
   bucket = local.bucket_name
 }
 
-# Проверка существования бакета
-locals {
-  bucket_exists = try(data.aws_s3_bucket.existing_bucket.id != "", false)
-}
-
 resource "aws_s3_bucket" "website_bucket" {
-  count = local.bucket_exists ? 0 : 1
+  count = length(data.aws_s3_bucket.existing_bucket) > 0 ? 0 : 1
 
   bucket = local.bucket_name
 
@@ -28,9 +23,9 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  count = local.bucket_exists ? 1 : 0
+  count = length(data.aws_s3_bucket.existing_bucket) > 0 ? 1 : 0
 
-  bucket = data.aws_s3_bucket.existing_bucket.id
+  bucket = data.aws_s3_bucket.existing_bucket[0].id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -38,7 +33,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         Effect = "Allow",
         Principal = "*",
         Action = "s3:GetObject",
-        Resource = "arn:aws:s3:::${data.aws_s3_bucket.existing_bucket.id}/*"
+        Resource = "arn:aws:s3:::${data.aws_s3_bucket.existing_bucket[0].id}/*"
       }
     ]
   })
@@ -47,13 +42,13 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "aws_s3_bucket_object" "html_files" {
   for_each = fileset("html_files", "*.html")
 
-  bucket = local.bucket_exists ? data.aws_s3_bucket.existing_bucket.id : aws_s3_bucket.website_bucket[0].id
+  bucket = length(data.aws_s3_bucket.existing_bucket) > 0 ? data.aws_s3_bucket.existing_bucket[0].id : aws_s3_bucket.website_bucket[0].id
   key    = each.key
   source = "html_files/${each.key}"
   acl    = "public-read"
 }
 
 output "website_url" {
-  value = local.bucket_exists ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket.website_bucket[0].website_endpoint
+  value = length(data.aws_s3_bucket.existing_bucket) > 0 ? data.aws_s3_bucket.existing_bucket[0].website_endpoint : aws_s3_bucket.website_bucket[0].website_endpoint
   description = "URL of the website hosted on S3"
 }
