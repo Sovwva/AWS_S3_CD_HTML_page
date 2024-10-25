7288280ef395e726523f317d67ab98919dd61806
sovwva <sovwva7@gmail.com>
2024-10-25 20:47:22 +0200

    terraform init

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "sovwva_aws_CD_html_bucket"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_object" "html_files" {
+  for_each = fileset("html_files", "*.html")
+
+  bucket = aws_s3_bucket.website_bucket.id
+  key    = each.key
+  source = "html_files/${each.key}"
+  acl    = "public-read"
+}
+
+output "website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+  description = "URL of the website hosted on S3"
+}
